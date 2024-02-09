e30eb8b32bf7220ada6dcf6a7ba71e8886c5e6fd
shiwangi <sshiwangi770@gmail.com>
2024-02-10 00:33:05 +0530

    added videoChat

---
+++ learn-ai-frontend/src/components/Input.tsx
@@ -0,0 +1,30 @@
+interface InputProps {
+  styles: string;
+  value: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  onClick?: () => void | Promise<void>;
+}
+
+export default function Input({
+  styles,
+  value,
+  onChange,
+  onClick = () => {},
+}: InputProps) {
+  return (
+    <div className="flex">
+      <input
+        className={styles}
+        placeholder="Your prompt here..."
+        value={value}
+        onChange={onChange}
+      />
+      <button
+        className="rounded-lg border border-transparent bg-custom-main px-3 py-1 text-slate-200 hover:bg-blue-700 focus:outline-none focus:ring-4 focus:ring-blue-300"
+        onClick={onClick}
+      >
+        Search
+      </button>
+    </div>
+  );
+}
---
+++ learn-ai-frontend/src/components/Prompt.tsx
@@ -0,0 +1,108 @@
+import { useState } from "react";
+import Message from "./Message";
+import Input from "./Input";
+
+interface PromptProps {
+  text: string;
+  id: string;
+}
+
+interface MessageItem {
+  role: string;
+  content: string;
+  answer?: string;
+}
+
+function Prompt({ text, id }: PromptProps) {
+  const [input, setInput] = useState<string>("");
+  const [messages, setMessages] = useState<MessageItem[]>([]);
+
+  const handleSubmit = async () => {
+    const prompt = {
+      role: "user",
+      content: input,
+      question: input,
+    };
+    setMessages([...messages, prompt]);
+
+    try {
+      const response = await fetch(
+        "https://learnai-api.vercel.app/api/AI/videoChat",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            id: id,
+            text: text,
+            question: input,
+          }),
+        }
+      );
+
+      const data = await response.json();
+      const answer = data.answer;
+      const assistantMessage: MessageItem = {
+        role: "assistant",
+        content: input, // Replace with the content you want to display
+        answer: answer, // Pass the user's question to assistant's message
+      };
+
+      // if (data.answer) {
+      //   assistantMessage.content = data.answer;
+      // }
+
+      setMessages([...messages, assistantMessage]);
+    } catch (error) {
+      console.error("Error fetching response:", error);
+    }
+
+    // await fetch("https://learnai-api.vercel.app/api/AI/videoChat", {
+    //   method: "POST",
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //   },
+    //   body: JSON.stringify({
+    //     id: id,
+    //     text: text,
+    //     question: input,
+    //   }),
+    // })
+    //   .then((data) => data.json())
+    //   .then((data) => {
+    //     console.log(data);
+    //     // Handle the API response
+    //   });
+    setInput("");
+  };
+  if (!text) {
+    return null;
+  }
+
+  return (
+    <div className="bg-custom-promptbg p-5 w-1/2 rounded-md">
+      <div>
+        <h3>start Chatting</h3>
+        <div>
+          {messages.map((el: MessageItem, i: number) => (
+            <Message
+              key={i}
+              role={el.role}
+              content={el.content}
+              answer={el.answer}
+            />
+          ))}
+        </div>
+        <Input
+          styles="w-full rounded-lg border border-slate-300 bg-slate-200 p-3 text-sm text-slate-800 shadow-md focus:border-blue-600 focus:outline-none focus:ring-1 focus:ring-blue-600 dark:border-slate-200/10 dark:bg-slate-800 dark:text-slate-200 dark:placeholder-slate-400 dark:focus:border-blue-600 sm:text-base"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          onClick={input ? handleSubmit : undefined}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Prompt;
